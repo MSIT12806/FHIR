af8872e4b8c1b65c54636e053a63c25b4af5b3c5
robin <alegalone@gmail.com>
2024-11-12 16:48:04 +0800

    換一個 swagger 套件讓 API 支持生成 XML

--- FHIR.WebSite/Program.cs
+++ FHIR.WebSite/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 namespace FHIR.WebSite
 {
     public class Program
@@ -8,14 +10,33 @@ namespace FHIR.WebSite
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
-            builder.Services.AddOpenApiDocument(); // 註冊服務加入 OpenAPI 文件
+            builder.Services
+                .AddControllers(o =>
+                {
+                    o.OutputFormatters.Add(new Microsoft.AspNetCore.Mvc.Formatters.XmlSerializerOutputFormatter()); // 自動使用默認的 XML 序列化設置
+                    o.RespectBrowserAcceptHeader = true; // 啟用對 Accept header 的支持
+                })
+                .AddXmlSerializerFormatters();  // 加入 XML 格式的支援(輸出 & 輸入都行)
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                // 指定支援 JSON 和 XML 格式的輸出
+                c.SupportNonNullableReferenceTypes();
+                //c.IncludeXmlComments("/swagger/v1/swagger.json");
+                // 添加 XML 和 JSON 的回應格式支援
+                c.MapType<string>(() => new OpenApiSchema { Type = "string", Format = "xml" });
+            });
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            app.UseOpenApi();    // 啟動 OpenAPI 文件
-            app.UseSwaggerUi(); // 啟動 Swagger UI
+            // 啟用 Swagger 中介軟體
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+                c.RoutePrefix = string.Empty; // 將 Swagger UI 設為應用根目錄
+            });
 
             app.UseHttpsRedirection();
 
