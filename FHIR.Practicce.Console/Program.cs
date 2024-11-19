e85d7e6f2e3e46b776e0011553c93505a3d38a73
robin <alegalone@gmail.com>
2024-11-19 10:19:13 +0800

    作業二

--- FHIR.Practicce.Console/Program.cs
+++ FHIR.Practicce.Console/Program.cs
@@ -1,4 +1,13 @@
 ﻿using FHIR.Tools;
+using Firely.Fhir.Packages;
+using Firely.Fhir.Validation;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+using Hl7.Fhir.Specification;
+using Hl7.Fhir.Specification.Source;
+using Hl7.Fhir.Specification.Terminology;
+using JsonDiffPatchDotNet;
+using Newtonsoft.Json.Linq;
 using System.Text;
 namespace FHIR.Practicce
 {
@@ -6,16 +15,137 @@ namespace FHIR.Practicce
     {
         const bool PRACTICE1 = true;
         const bool PRACTICE2 = true;
+        const bool PRACTICE2_5 = true;
         const bool PRACTICE3 = true;
         const bool PRACTICE4 = true;
         const bool PRACTICE5 = true;
         const bool PRACTICE6 = true;
-        static async Task Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
-            if (PRACTICE1 == false)
+            if (PRACTICE1)
             {
                 var p = Homework.GetPatientData();
-                Console.WriteLine(p);
+
+                const string answer = @"
+{
+  ""resourceType"": ""Patient"",
+  ""identifier"": [
+    {
+      ""use"": ""official"",
+      ""type"": {
+        ""coding"": [
+          {
+            ""system"": ""http://terminology.hl7.org/CodeSystem/v2-0203"",
+            ""code"": ""PPN"",
+            ""display"": ""Passport Number""
+          }
+        ]
+      },
+      ""value"": ""65848725""
+    }
+  ],
+  ""name"": [
+    {
+      ""use"": ""official"",
+      ""text"": ""Connor Graham"",
+      ""family"": ""Graham"",
+      ""given"": [
+        ""Connor""
+      ]
+    },
+    {
+      ""use"": ""official"",
+      ""text"": ""葛康納""
+    }
+  ],
+  ""telecom"": [
+    {
+      ""system"": ""phone"",
+      ""value"": ""07-2159685"",
+      ""use"": ""home""
+    },
+    {
+      ""system"": ""phone"",
+      ""value"": ""07-7938888"",
+      ""use"": ""work""
+    },
+    {
+      ""system"": ""phone"",
+      ""value"": ""0912-354879"",
+      ""use"": ""mobile""
+    }
+  ],
+  ""gender"": ""male"",
+  ""address"": [
+    {
+      ""use"": ""home"",
+      ""type"": ""both"",
+      ""text"": ""高雄市橋頭區經武路58號24樓之11"",
+      ""line"": [
+        ""經武路58號24樓之11""
+      ],
+      ""city"": ""高雄市"",
+      ""district"": ""橋頭區"",
+      ""postalCode"": ""825006"",
+      ""country"": ""TW""
+    }
+  ],
+  ""contact"": [
+    {
+      ""relationship"": [
+        {
+          ""coding"": [
+            {
+              ""system"": ""http://terminology.hl7.org/CodeSystem/v2-0131"",
+              ""code"": ""C"",
+              ""display"": ""Emergency Contact""
+            }
+          ],
+          ""text"": ""父""
+        }
+      ],
+      ""name"": {
+        ""use"": ""official"",
+        ""text"": ""James Yates""
+      },
+      ""telecom"": [
+        {
+          ""system"": ""phone"",
+          ""value"": ""0988-878545"",
+          ""use"": ""mobile""
+        }
+      ]
+    }
+  ],
+  ""photo"": [
+    {
+      ""url"": ""https://i.imgur.com/VeTQheO.png""
+    }
+  ],
+  ""communication"": [
+    {
+      ""language"": {
+        ""coding"": [
+          {
+            ""system"": ""urn:ietf:bcp:47"",
+            ""code"": ""en-US""
+          }
+        ],
+        ""text"": ""English (US)""
+      }
+    }
+  ]
+}";
+
+                // 使用 jsondiffpatch.net 套件比對答案與您的 Patient 資料差異
+                var jdp = new JsonDiffPatch();
+                var jTokenAnswer = JToken.Parse(answer);
+                var jTokenPatient = JToken.Parse(Homework.GetPatientJson());
+                var patch = jdp.Diff(jTokenPatient, jTokenAnswer);
+                var diff = jdp.Patch(jTokenPatient, patch);
+
+                Console.WriteLine(Homework.GetPatientJson());
+
             }
 
             if (PRACTICE2)
@@ -52,7 +182,88 @@ namespace FHIR.Practicce
                         Console.WriteLine("上傳失敗。");
                         Console.WriteLine("Status Code: " + response.StatusCode);
                         string errorBody = await response.Content.ReadAsStringAsync();
-                        Console.WriteLine("Error Message: " + errorBody);
+                        //解析 FHIR OperationOutcome 資料
+                        var outcome = new FhirJsonParser().Parse<OperationOutcome>(errorBody);
+                        foreach (var issue in outcome.Issue)
+                        {
+                            if (issue.Severity == OperationOutcome.IssueSeverity.Error)
+                            {
+                                Console.WriteLine($"Severity: {issue.Severity}");
+                                Console.WriteLine($"Code: {issue.Code}");
+                                Console.WriteLine($"Diagnostics: {issue.Diagnostics}");
+                                // 如果有 `details` 的額外編碼，則列出
+                                if (issue.Details != null && issue.Details.Coding != null)
+                                {
+                                    foreach (var coding in issue.Details.Coding)
+                                    {
+                                        Console.WriteLine($"Details Code System: {coding.System}, Code: {coding.Code}");
+                                    }
+                                }
+
+                                // 顯示相關位置
+                                if (issue.Location != null)
+                                {
+                                    Console.WriteLine("Location(s): " + string.Join(", ", issue.Location));
+                                }
+
+                                Console.WriteLine("------");
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (PRACTICE2_5 == false)
+            {
+                // 1. FHIR Server URL (您需要替換為實際的 FHIR Server URL)
+                const string profileUrl = "https://hapi.fhir.tw/fhir/StructureDefinition/TWCorePatient\"";
+
+                // 2. 設定 Patient Resource 資料
+                var patient = Homework.GetPatientData();
+
+                var packageServer = "https://hapi.fhir.tw";
+                var fhirRelease = FhirRelease.R4;
+                var packageResolver = FhirPackageSource.CreateCorePackageSource(ModelInfo.ModelInspector, fhirRelease, packageServer);
+                var resourceResolver = new CachedResolver(packageResolver);
+                var terminologyService = new LocalTerminologyService(resourceResolver);
+
+                var validator = new Validator(resourceResolver, terminologyService);
+                var profile = "https://hapi.fhir.tw/fhir/StructureDefinition/TWCorePatient";
+                var result = validator.Validate(patient, profile);
+
+
+                // 確認回應結果
+                if (result.Success)
+                {
+                    Console.WriteLine("Patient Resource 已成功上傳！");
+                }
+                else
+                {
+                    Console.WriteLine("上傳失敗。");
+                    foreach (var issue in result.Issue)
+                    {
+                        if (issue.Severity == OperationOutcome.IssueSeverity.Error)
+                        {
+                            Console.WriteLine($"Severity: {issue.Severity}");
+                            Console.WriteLine($"Code: {issue.Code}");
+                            Console.WriteLine($"Diagnostics: {issue.Diagnostics}");
+                            // 如果有 `details` 的額外編碼，則列出
+                            if (issue.Details != null && issue.Details.Coding != null)
+                            {
+                                foreach (var coding in issue.Details.Coding)
+                                {
+                                    Console.WriteLine($"Details Code System: {coding.System}, Code: {coding.Code}");
+                                }
+                            }
+
+                            // 顯示相關位置
+                            if (issue.Location != null)
+                            {
+                                Console.WriteLine("Location(s): " + string.Join(", ", issue.Location));
+                            }
+
+                            Console.WriteLine("------");
+                        }
                     }
                 }
             }
--- FHIR.Tools/Class1.cs
+++ FHIR.Tools/Class1.cs
@@ -4,7 +4,36 @@ using Hl7.Fhir.Validation;
 
 namespace FHIR.Tools
 {
+    public static class PatientHelper
+    {
+        public static class AddressHelper
+        {
+            public static Extension PostalCodeExtension(string code)
+            {
+                // 創建 Coding 對象並設置系統和代碼
+                var postalCodeCoding = new Coding
+                {
+                    System = "https://twcore.mohw.gov.tw/ig/twcore/CodeSystem/postal-code3-tw",
+                    Code = code
+                };
 
+                // 創建 CodeableConcept 並將 Coding 添加到其中
+                var postalCodeCodeableConcept = new CodeableConcept
+                {
+                    Coding = new List<Coding> { postalCodeCoding }
+                };
+
+                // 創建 Extension 並設置 URL 和 valueCodeableConcept
+                var postalCodeExtension = new Extension
+                {
+                    Url = "https://twcore.mohw.gov.tw/ig/twcore/StructureDefinition/tw-postal-code",
+                    Value = postalCodeCodeableConcept
+                };
+
+                return postalCodeExtension;
+            }
+        }
+    }
     public static class Homework
     {
         public static Patient GetPatientData()
@@ -22,6 +51,18 @@ namespace FHIR.Tools
 
             Patient patient = new Patient();
 
+            patient.Meta = new Meta
+            {
+                Profile = new string[] {
+                    //"https://twcore.mohw.gov.tw/ig/pas/StructureDefinition/Patient-twpas",
+                    //"https://hapi.fhir.tw/fhir/StructureDefinition/MITW-T1-SC2-PatientIdentification",
+                    //"https://hapi.fhir.tw/fhir/StructureDefinition/MITW-T1-SC1-PatientCore",
+                    //"https://hapi.fhir.tw/fhir/StructureDefinition/MITW-T1-SC3-PatientContact",
+                    "https://hapi.fhir.tw/fhir/StructureDefinition/TWCorePatient", //卡一個「找不到 profile https://twcore.mohw.gov.tw/ig/twcore/0.2.1/StructureDefinition-Address-twcore.html 」
+                }
+
+            };
+
             //-病人英文姓名為「Connor Graham」，中譯姓名為「葛康納」
             patient.Name = new List<HumanName>
             {
@@ -29,14 +70,35 @@ namespace FHIR.Tools
                 {
                     Use = HumanName.NameUse.Official,
                     Family = "Graham",
-                    Given = new string[] { "Connor" }
+                    Given = new string[] { "Connor" },
+                    Text = "Connor Graham"
                 },
                 new HumanName
                 {
                     Use = HumanName.NameUse.Usual,
+                    //Use = HumanName.NameUse.Official,
                     Text = "葛康納"
                 }
             };
+
+            //-這是他的照片：https://i.imgur.com/VeTQheO.png
+            patient.Photo = new List<Attachment>
+            {
+                new Attachment
+                {
+                    Url = "https://i.imgur.com/VeTQheO.png"
+                 }
+            };
+
+            // birthDate
+            patient.BirthDateElement = new Date();
+            patient.BirthDateElement.Extension.Add(new Extension
+            {
+                Url = "http://hl7.org/fhir/StructureDefinition/data-absent-reason",
+                Value = new Code("unknown")
+            });
+
+            patient.Gender = AdministrativeGender.Male;
 
             //        -護照號碼：65848725
             patient.Identifier = new List<Identifier>
@@ -44,7 +106,19 @@ namespace FHIR.Tools
                 new Identifier
                 {
                     Use = Identifier.IdentifierUse.Official,
-                    System = "http://terminology.hl7.org/CodeSystem/v2-0203", //衛服部編碼?
+                    Type = new CodeableConcept
+                    {
+                        Coding = new List<Coding>
+                        {
+                            new Coding
+                            {
+                                System = "http://terminology.hl7.org/CodeSystem/v2-0203", //衛服部編碼?
+                                Code = "PPN",
+                                //Display = "Passport Number"
+                            }
+                        }
+                    },
+                    System = "http://www.boca.gov.tw",//明明是 0..1，為啥跳錯誤？而且 uri 要寫啥我也不知道 //喔，profile 已經限定了
                     Value = "65848725",
                 }
             };
@@ -73,16 +147,32 @@ namespace FHIR.Tools
             };
 
             //        - 聯絡地址：高雄市橋頭區經武路 58 號 24 樓之 11
-            patient.Address = new List<Address>
-            {
-                new Address
-                {
-                    Use = Address.AddressUse.Home,
-                    Line = new string[] { "經武路 58 號 24 樓之 11" },
-                    City = "高雄市",
-                    District = "橋頭區" //district 可能是美國的"州"，所以應該不會是橋頭區
-                }
-            };
+            // 地址一直驗不過...乾脆先註解掉
+            //patient.Address = new List<Address>
+            //{
+            //    new Address
+            //    {
+            //        Use = Address.AddressUse.Home,
+            //        District = "橋頭區", //district 可能是美國的"州"，所以應該不會是橋頭區
+            //        Country = "TW",
+            //        City = "高雄市",
+
+            //        PostalCode = "825006",
+            //        Extension = new List<Extension>
+            //        {
+            //            PatientHelper.AddressHelper.PostalCodeExtension("825006")
+            //        },
+
+            //        Line = new string[] { "經武路 58 號 24 樓之 11" },
+            //    }
+            //};
+
+            // 更換 profile
+            //patient.Address[0].= new List<string>
+            //{
+            //    // 替換為伺服器支援的地址 Profile 或移除此行
+            //    "https://twcore.mohw.gov.tw/ig/twcore/0.2.1/StructureDefinition-Address-twcore.html"
+            //};
 
             //- 緊急聯絡人姓名：James Yates   緊急聯絡人電話：(手機)0988 - 878545 緊急聯絡人關係：父
             patient.Contact = new List<Patient.ContactComponent>
@@ -93,6 +183,15 @@ namespace FHIR.Tools
                     {
                         new CodeableConcept
                         {
+                            Coding = new List<Coding>
+                            {
+                                new Coding
+                                {
+                                    System = "http://terminology.hl7.org/CodeSystem/v2-0131",
+                                    Code = "C",
+                                    Display = "Emergency Contact"
+                                }
+                            },
                             Text = "父"
                         }
                     },
@@ -127,7 +226,8 @@ namespace FHIR.Tools
                                 Code = "en-US"
                             }
                         }
-                    }
+                    },
+                    Preferred = true
                 }
             };
 
